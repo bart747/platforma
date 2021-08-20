52e848d57f67b8ef285ff46ecd3653f9b74aba61
Bartosz Wieczorek <hello.bartosz@gmail.com>
2021-08-21 01:02:47 +0200

    ui edits, header animation redesign

--- components/headerHome.tsx
+++ components/headerHome.tsx
@@ -1,47 +1,51 @@
 function HeaderHome () {
   return (
-    <header className="bg-gray-900 text-green-400 font-mono rounded-md">
-      <div className="header-content bg-gray-900 px-4 py-6 rounded-md text-xl font-normal">
-        <h1 className="text-xl mt-4 mb-8 font-normal">Making Visually Rich, <br></br> yet Fast Web UIs</h1>
-        <h2 className="text-xl mt-4 mb-4 font-normal">SSR, DataViz, SVG+JS</h2>
+    <header className="bg-gray-900 max-w-xl text-green-400 font-mono rounded-md">
+      <div className="header-content bg-gray-900 px-4 py-20 rounded-md text-xl font-normal">
+        <h1 className="text-lg mt-4 mb-8 font-normal">* Making Visually Rich, yet Fast Web UIs *</h1>
+        <h2 className="text-lg mt-4 mb-4 font-normal">[ SSR, DataViz, SVG+JS ]</h2>
       </div>
       <style jsx>{`
         header {
-          background-image: url("./pcbbckcomp.jpg");
-          background-position: center;
-          background-size: 35%;
+          background-image: url("./glitch.svg");
+          background-position: top;
+          background-size: 33%;
           box-shadow: inset 0 -1px 10px #777;
         }
 
         .header-content {
           animation-name: flicker;
-          animation-duration: 1.5s;
-          animation-iteration-count: 2;
+          animation-duration: 1s;
+          box-shadow: inset 0 0 100px #252;
         }
 
         @keyframes flicker {
           from {
             opacity: 1;
+            color: blue; 
           }
 
-          50% {
-            opacity: 1;
+          33% {
+            opacity: 0.7;
+            color: blue; 
           }
 
           60% {
-            opacity: 0.7;
+            opacity: 1;
+            color: #121; 
           }
           
           65% {
             opacity: 1;
           }
 
-          70% {
+          75% {
             opacity: 0.8;
           }
 
-          75% {
+          80% {
             opacity: 1;
+            color: green;
           }
 
           to {
