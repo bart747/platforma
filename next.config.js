3a1d9df5d2a5cb1d432303ff202ef9eced9af8e4
Bartosz Wieczorek <hello.bartosz@gmail.com>
2021-08-21 01:05:40 +0200

    specify default language in config

--- next.config.js
+++ next.config.js
@@ -7,5 +7,13 @@ module.exports = {
       },
    )
     return config
+  },
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US',
   }
 }
