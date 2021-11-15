da2d2a73a7c5a0d9a27dc3b07e18e8d394a867a0
Bartosz Wieczorek <hello.bartosz@gmail.com>
2021-11-15 21:17:26 +0100

    ui edit, style related

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,15 @@
 function Footer () {
   return (
-   <footer className="mt-8 text-sm mx-auto max-w-xl">
-     <p>
-       ©2016-2021 Bartosz Wieczorek.<br />
-       Some rights reserved — <a
-         href="https://creativecommons.org/licenses/by/4.0/">
-       CC BY 4.0
-       </a> — please attribute and link back.
-     </p>
+    <footer className="m-8 text-sm mx-auto max-w-xl">
+      <div>
+        ©2016-2021 Bartosz Wieczorek.
+      </div>
+      <div>
+        Some rights reserved — <a
+          href="https://creativecommons.org/licenses/by/4.0/">
+            CC BY 4.0
+        </a> — please attribute and link back.
+     </div>
    </footer>
   )
 }
--- components/headerHome.tsx
+++ components/headerHome.tsx
@@ -2,10 +2,18 @@ import styles from './header.module.css'
 
 function HeaderHome () {
   return (
-    <header className="bg-image-trees bg-cover rounded-full w-64 h-64 mx-auto blur-lg">
-      <div className="rounded-full w-64 h-64">
-        <h1 className="font-bold text-red-500 text-center text-3xl m-0 py-28">I∴X</h1>
+    <header>
+      <div className="bg-image-trees bg-cover rounded-full w-64 h-64 mx-auto blur-lg">
+        <div className="bg-gradient-to-r from-yellow-400 via-white to-blue-500 opacity-50
+                        rounded-full w-64 h-64">
+          <div className="font-mono text-black text-center text-2xl m-0 py-24 leading-5">
+            <div className="">U</div><div className="">I</div>
+            <div className="">∴</div>
+            <div className="">X</div>
+          </div>
+        </div>
       </div>
+      <h1 className="text-center font-normal text-gray-600 text-2xl tracking-tight ">clean<br />modular<br />interfaces</h1>
     </header>
   )
 }
