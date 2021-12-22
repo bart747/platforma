9aa314b65ecd05d7515ef0c25be82ebb4b0f3189
Bartosz Wieczorek <hello.bartosz@gmail.com>
2021-12-22 02:05:03 +0100

    add path matching to config, trailing slash related

--- next.config.js
+++ next.config.js
@@ -8,5 +8,14 @@ module.exports = {
       },
    )
     return config
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:slug*/',
+        destination: '/:slug*', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
 }
