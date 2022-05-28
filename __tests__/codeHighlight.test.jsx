d862949aa67958940b2ce60d53413e14b1e7c72f
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-05-28 17:49:56 +0200

    refactor tests for new version of highlighter

--- __tests__/codeHighlight.test.jsx
+++ __tests__/codeHighlight.test.jsx
@@ -1,26 +1,18 @@
 // __tests__/codeHighlight.test.jsx
 
 import Highlight from '../util/codeHighlight'
-//  String formating is really important
+//  String formatting is really important
 //  – remember about newlines, quotes, trailing spaces
-const snippet = `
-(a) => a + 1;
-`
+const snippet = "(a) => a + 1;"
 const snippetF = Highlight(snippet)
-const expectation = `
-<span style='color:#665'>(</span>a<span style='color:#665'>)</span> <span style='color:#832'>=></span> a + 1<span style='color:#665'>;</span>
-`
-test('formated code snipped equals test sample', () => {
+const expectation = "<pre><code class='text-amber-700'> <span class='text-gray-500'>(</span>a<span class='text-gray-500'>)</span> <span class='text-brand-maroon'>=></span> a + 1<span class='text-gray-500'>;</span>"
+test('formatted code snipped equals test sample', () => {
   expect(snippetF).toMatch(expectation);
 });
 
-const snippet2 = `
-return // abc
-`
+const snippet2 = "return // abc"
 const snippetF2 = Highlight(snippet2)
-const expectation2 = `
-<span style='color:#832'>return</span> <span style='color:#665'>// abc</span>
-`
-test('formated code snipped equals test sample (v2)', () => {
+const expectation2 = "<pre><code class='text-amber-700'> return <span class='text-gray-500'>// abc</span> </code></pre>"
+test('formatted code snipped equals test sample (v2)', () => {
   expect(snippetF2).toMatch(expectation2);
 });
