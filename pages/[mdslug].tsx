e5974a550bdb0588d8f851a52ad6993ef52dc9c6
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-06-20 21:23:14 +0200

    add moer strict types to 'mdslug', run build

--- pages/[mdslug].tsx
+++ pages/[mdslug].tsx
@@ -1,22 +1,28 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
 import ArticlePage from '../components/article'
 import codeHighlight from '../util/codeHighlight'
 
-function article ({ title, content }) {
+function article (props:ArticleProps) {
   return (
     <ArticlePage
-      title={ title }
-      content={ content }
+      title={ props.title }
+      content={ props.content }
     />
   )
 }
 
-export async function getStaticProps ({ params }) {
+interface ArticleProps {
+  title: string;
+  content: string;
+}
+
+export const getStaticProps: GetStaticProps = async ( { params }: any ) => {
   const fileName: string = `${params.mdslug}.md`
   const res: any = await fetch(`https://raw.githubusercontent.com/bart747/notebook/main/articles/${fileName}`)
   const text: string = await res.text()
 
   const content = text.replace(/\`\`\`([\s\S]*?)\`\`\`/g, codeHighlight)
-  const title: string = text.split('\n')[0].replace('# ', '')
+  const title = text.split('\n')[0].replace('# ', '')
 
   return {
     props: {
@@ -26,7 +32,7 @@ export async function getStaticProps ({ params }) {
   }
 }
 
-export async function getStaticPaths () {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [
       { params: { mdslug: 'complexity' } },
