84ebbf5be0d69a66484f822c2782ccfc6f5601fd
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-07-30 01:44:45 +0200

    refactor and update link at 'mdslug' (content fetching)

--- pages/[mdslug].tsx
+++ pages/[mdslug].tsx
@@ -2,6 +2,11 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 import ArticlePage from '../components/article'
 import codeHighlight from '../util/codeHighlight'
 
+interface ArticleProps {
+  title: string;
+  content: string;
+}
+
 function article (props:ArticleProps) {
   return (
     <ArticlePage
@@ -11,14 +16,9 @@ function article (props:ArticleProps) {
   )
 }
 
-interface ArticleProps {
-  title: string;
-  content: string;
-}
-
 export const getStaticProps: GetStaticProps = async ( { params }: any ) => {
   const fileName: string = `${params.mdslug}.md`
-  const res: any = await fetch(`https://raw.githubusercontent.com/bart747/notebook/main/articles/${fileName}`)
+  const res: any = await fetch(`https://raw.githubusercontent.com/bart747/notebook/96309a512ab5459d6b9c09582f9734bda8c75d56/articles/${fileName}`)
   const text: string = await res.text()
 
   const content = text.replace(/\`\`\`([\s\S]*?)\`\`\`/g, codeHighlight)
