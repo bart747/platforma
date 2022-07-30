f91718e3f035984f70ed5f94f8177675c9a5a495
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-07-30 18:30:06 +0200

    clean up header component code

--- components/headerImage.tsx
+++ components/headerImage.tsx
@@ -1,13 +1,11 @@
 function HeaderImage() {
   return (
     <header>
-      <div className="">
         <h1 className="font-bold text-center text-black text-6xl m-0 py-14 leading-none">
           Modular<br />
           Light<br />
           UIs
         </h1>
-      </div>
     </header>
   )
 }
