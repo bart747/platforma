4422c44c9d2f9d5008529fc0fd4530b51be1249f
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-07-30 19:57:17 +0200

    change color of header text

--- components/headerImage.tsx
+++ components/headerImage.tsx
@@ -1,11 +1,11 @@
 function HeaderImage() {
   return (
-    <header>
-        <h1 className="font-bold text-center text-black text-6xl m-0 py-14 leading-none">
-          Modular<br />
-          Light<br />
-          UIs
-        </h1>
+    <header className="">
+      <h1 className="max-w-md mx-auto bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 to-blue-800 font-bold text-center text-6xl m-0 py-14 leading-none">
+        Modular<br />
+        Light<br />
+        UIs
+      </h1>
     </header>
   )
 }
