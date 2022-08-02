6b60e3c4cadc31b71d03bbff98f2823d8a5e116b
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-08-03 00:14:07 +0200

    change colors of 'headerImage'

--- components/headerImage.tsx
+++ components/headerImage.tsx
@@ -1,7 +1,7 @@
 function HeaderImage() {
   return (
     <header className="">
-      <h1 className="max-w-md mx-auto bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 to-blue-800 font-bold text-center text-6xl m-0 py-14 leading-none">
+      <h1 className="max-w-md mx-auto bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-orange-800 font-bold text-center text-6xl m-0 py-14 leading-none">
         Modular
         <br />
         Light
