befa489acfda506a965af32b0ea02ffc96397d80
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-09-08 22:46:25 +0200

    remove unnecessary whitespace

--- components/articleList.tsx
+++ components/articleList.tsx
@@ -75,7 +75,7 @@ const articles = [
       <rect x="2" y="10" width="7" height="5" rx="0.6" transform="rotate(-90 2 10)" stroke="currentColor" stroke-width="1.5"/>
       <path d="M7 17.5H10.5C11.6046 17.5 12.5 16.6046 12.5 15.5V8.5C12.5 7.39543 11.6046 6.5 10.5 6.5H7" stroke="currentColor" stroke-width="1.5"/>
       <path d="M12.5 12H17" stroke="currentColor" stroke-width="1.5"/>
-      </svg>    
+      </svg>
     `,
   },
   {
