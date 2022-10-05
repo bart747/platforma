29ca3e80863e78278b9d649116efeea2810cddc3
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-10-06 01:53:32 +0200

    refactor growthCalc

--- components/growthCalc.tsx
+++ components/growthCalc.tsx
@@ -16,62 +16,71 @@ interface OutputObj {
   average: string;
 }
 
-class GrowthCalc extends Component<{}, { value: string[] }> {
+class GrowthCalc extends Component<{}, { value: number[], out: any }> {
   constructor(props: any) {
     super(props);
-    this.state = { value: [] };
+    this.state = { value: [], out: {} };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event: any) {
-    this.setState({ value: event.target.value.split(/[,;|/]/) });
+    this.setState({
+      value: event.target.value
+        .split(/[,;|/]/)
+        .filter((el: any) => el !== "" && el !== " ")
+        .map((el: any) => Number(el)),
+    });
   }
 
-  FmtOutput(inputArr: string[]): OutputObj {
-    const arrStr: string[] = inputArr.filter((n) => n !== "" && n !== " ");
-
-    if (arrStr.length <= 1 || arrStr[1] === "" || arrStr[1] === " ") {
-      return {
-        compound: "",
-        startToEnd: "",
-        yearToYear: { __html: [] },
-        average: "",
-      };
+  compound(inputArr: number[]): string {
+    if (inputArr.length <= 1) {
+      return "";
     }
-    if (arrStr[arrStr.length - 1] === "" || arrStr[arrStr.length - 1] === " ") {
-      arrStr.pop();
-    }
+    const years = inputArr.length - 1;
+    const begin: number = inputArr[0];
+    const final: number = inputArr[inputArr.length - 1];
 
-    const arrNum = arrStr.map((el) => Number(el));
-    const years = arrNum.length - 1;
-    const begin: number = arrNum[0];
-    const final: number = arrNum[arrNum.length - 1];
-
-    let compound: any = GrowthCompound(begin, final, years);
-    if (isNaN(compound)) {
-      compound = "unsupported input";
-    } else if (compound === Infinity) {
-      compound = ` ∞`;
-    } else if (compound === -Infinity) {
-      compound = ` -∞`;
+    let growth: number = GrowthCompound(begin, final, years);
+    let result: string;
+    if (isNaN(growth)) {
+      result = "unsupported input";
+    } else if (growth === Infinity) {
+      result = ` ∞`;
+    } else if (growth === -Infinity) {
+      result = ` -∞`;
     } else {
-      compound = compound.toFixed(3) + "%";
+      result = growth.toFixed(3) + "%";
     }
+    return result;
+  }
 
-    let startToEnd: any = GrowthStartToEnd(begin, final);
-    if (isNaN(startToEnd)) {
-      startToEnd = "unsupported input";
-    } else if (startToEnd === Infinity) {
-      startToEnd = ` ∞`;
-    } else if (startToEnd === -Infinity) {
-      startToEnd = ` -∞`;
+  startToEnd(inputArr: number[]): string {
+    if (inputArr.length <= 1) {
+      return "";
+    }
+    const begin: number = inputArr[0];
+    const final: number = inputArr[inputArr.length - 1];
+
+    let growth: number = GrowthStartToEnd(begin, final);
+    let result: string;
+    if (isNaN(growth)) {
+      result = "unsupported input";
+    } else if (growth === Infinity) {
+      result = ` ∞`;
+    } else if (growth === -Infinity) {
+      result = ` -∞`;
     } else {
-      startToEnd = startToEnd.toFixed(3) + "%";
+      result = growth.toFixed(3) + "%";
     }
-
-    const yearToYearNums: number[] = GrowthYY(arrNum);
-
-    const yearToYear: string[] = yearToYearNums.map((el, i) => {
+    return result;
+  }
+  
+  yearToYear(inputArr: number[]): any {
+    if (inputArr.length <= 1) {
+      return [];
+    }
+    const yearToYearArr: number[] = GrowthYY(inputArr);
+    const result: string[] = yearToYearArr.map(el => {
       if (isNaN(el)) {
         return "unsupported input";
       } else if (el === Infinity) {
@@ -82,31 +91,32 @@ class GrowthCalc extends Component<{}, { value: string[] }> {
         return ` ${el.toFixed(3)}%`;
       }
     });
+    return result;
+  }
 
-    let averageNum: number = 0;
-    let average: string = "";
-    if (yearToYearNums.length > 1) {
-      averageNum = GrowthAverage(yearToYearNums);
+  average(inputArr: number[]): any {
+    if (inputArr.length <= 1) {
+      return [];
     }
-    if (yearToYearNums.length === 1) {
-      averageNum = yearToYearNums[0];
+    const yearToYearArr: number[] = GrowthYY(inputArr);
+    let averageNum: number = 0;
+    let result: string = "";
+    if (yearToYearArr.length > 1) {
+      averageNum = GrowthAverage(yearToYearArr);
+    }
+    if (yearToYearArr.length === 1) {
+      averageNum = yearToYearArr[0];
     }
     if (isNaN(averageNum)) {
-      average = "unsupported input";
+      result = "unsupported input";
     } else if (averageNum === Infinity) {
-      average = ` ∞`;
+      result = ` ∞`;
     } else if (averageNum === -Infinity) {
-      average = ` -∞`;
+      result = ` -∞`;
     } else {
-      average = averageNum.toFixed(3) + "%";
+      result = averageNum.toFixed(3) + "%";
     }
-
-    return {
-      compound: compound,
-      startToEnd: startToEnd,
-      yearToYear: { __html: yearToYear },
-      average: average,
-    };
+    return result
   }
 
   render() {
@@ -119,7 +129,6 @@ class GrowthCalc extends Component<{}, { value: string[] }> {
             </div>
             <input
               className="text-lg font-mono caret-blue-600 border-2 border-gray-600 px-1.5 py-1 min-w-full rounded-sm"
-              value={this.state.value}
               onChange={this.handleChange}
               placeholder="110, 120, 130, 140, 150"
               autoFocus
@@ -136,21 +145,21 @@ class GrowthCalc extends Component<{}, { value: string[] }> {
             Compound Annual Growth Rate (CAGR):
           </h3>
           <div className="font-mono text-lg bg-amber-100 inline-block min-h-[2.5rem] min-w-[14rem] px-2 py-1.5">
-            {this.FmtOutput(this.state.value).compound}
+            {this.compound(this.state.value)}
           </div>
           <h3 className="text-base mt-4 mb-1 ">Beginning to End:</h3>
           <div className="font-mono text-lg bg-amber-100 inline-block min-h-[2.5rem] min-w-[14rem] px-2 py-1.5">
-            {this.FmtOutput(this.state.value).startToEnd}
+            {this.startToEnd(this.state.value)}
           </div>
           <h3 className="text-base mt-4 mb-1 ">Average of Y/Y:</h3>
           <div className="font-mono text-lg bg-amber-100 inline-block min-h-[2.5rem] min-w-[14rem] px-2 py-2">
-            {this.FmtOutput(this.state.value).average}
+            {this.average(this.state.value)}
           </div>
           <h3 className="text-base mt-4 mb-1 ">Y/Y List:</h3>
           <div className="font-mono text-md bg-amber-100 inline-block min-h-[2.5rem] min-w-[14rem] px-2 py-2">
             <div
               dangerouslySetInnerHTML={
-                this.FmtOutput(this.state.value).yearToYear
+                { __html: this.yearToYear(this.state.value) }
               }
             />
           </div>
