e7330c49cae6244fc4d380409a0e368ef5102193
Bartosz Wieczorek <hello.bartosz@gmail.com>
2022-11-19 01:05:47 +0100

    remove unused and redundant config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  swcMinify: true,
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader",
-    });
-    return config;
-  },
-};
+module.exports = {};
