1a8f211dea641a635360efb4693d892f1bad3e51
Bartosz Wieczorek <hello.bartosz@gmail.com>
2023-07-20 15:27:59 +0200

    redesign header image

--- components/headerImage.tsx
+++ components/headerImage.tsx
@@ -2,7 +2,7 @@ function HeaderImage() {
   return (
     <header className="">
       <div className="animation-symbol container mx-auto mt-12 mb-16"></div>
-      <h1 className="max-w-md mx-auto bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-orange-800 font-bold text-center text-6xl m-0 my-14 leading-none">
+      <h1 className="max-w-md mx-auto font-bold text-center text-6xl m-0 my-14 leading-none">
         Modular
         <br />
         Light
