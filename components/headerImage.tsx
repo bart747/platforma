f6c024d1eb50adb87b59c2d00b798245b96f1d6d
Bartosz Wieczorek <hello.bartosz@gmail.com>
2023-09-01 22:56:06 +0200

    minor ui style edit

--- components/headerImage.tsx
+++ components/headerImage.tsx
@@ -4,7 +4,7 @@ function HeaderImage() {
       <div className="animation-symbol bg-transparent container w-20 h-20 mx-auto mt-12 mb-16 p-6">
         <div className="bg-black mx-auto my-auto w-8 h-8 rounded-full align-middle"></div>
       </div>
-      <h1 className="max-w-md mx-auto font-bold text-center text-6xl m-0 my-14 leading-none">
+      <h1 className="max-w-md mx-auto font-bold text-center text-white text-6xl m-0 my-14 leading-none">
         Modular
         <br />
         Light
